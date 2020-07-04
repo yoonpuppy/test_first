30d86e758710db91b34e6151fe191bee4ef97673
parkcheodol <3611345@naver.com>
2020-07-05 01:27:10 +0900

    200705 1:27

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ app.post('/register', (req, res) => {
   //save 하기 전에 암호화 시켜야 함 #10 bcrypt
   //User.js modified
   
-  //1:13
-
   //mongoDB method
   user.save((err, doc) => {
     if(err) return res.json({ success: false, err })
